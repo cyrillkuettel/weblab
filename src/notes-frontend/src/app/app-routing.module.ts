d80cc04450ee3e250376c66830afbbd415dbf540
Cyrill Küttel <cyrillkuettel@gmail.com>
2023-02-19 19:30:48 +0100

    Generate initial boilerplate and bulma

---
+++ src/notes-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {NotesListComponent} from "./pages/notes-list/notes-list.component";
+
+const routes: Routes = [
+  { path: '', component: NotesListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
