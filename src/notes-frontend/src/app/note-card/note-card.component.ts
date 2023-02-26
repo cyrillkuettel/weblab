d1e2883cc2e60a862727dd341cce06d3ae49ca2c
Cyrill <cyrillkuettel@gmail.com>
2023-02-26 15:12:57 +0100

    Add property binding for very simple note

--- src/notes-frontend/src/app/note-card/note-card.component.ts
+++ src/notes-frontend/src/app/note-card/note-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent {
+export class NoteCardComponent implements OnInit{
+  @Input('title') title: string | undefined;
+  @Input('content') content: string | undefined;
+
+  ngOnInit(): void {
+  }
 
 }
