50728b5dbbf45f464a6a0e06f94204dbeff1dd5a
Cyrill <cyrillkuettel@gmail.com>
2023-02-27 15:45:38 +0100

    Add naive Note model and listen for events in onSubmit
    Just console.log(note) for now, saving will be added later

--- src/notes-frontend/src/app/pages/note-details/note-details.component.ts
+++ src/notes-frontend/src/app/pages/note-details/note-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Note} from "../../models/node.model";
 
 @Component({
   selector: 'app-note-details',
   templateUrl: './note-details.component.html',
   styleUrls: ['./note-details.component.scss']
 })
-export class NoteDetailsComponent {
+export class NoteDetailsComponent implements OnInit{
+  note: Note | undefined;
 
+
+  onSubmit(noteForm: NgForm) {
+    console.log(noteForm);
+  }
+
+  ngOnInit(): void {
+    this.note = new Note();
+  }
 }
